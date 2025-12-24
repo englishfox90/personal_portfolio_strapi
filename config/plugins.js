9bf3140a85da6d79f1cdaf839afa80116e1a17e3
Paul Fox-Reeks <pfoxreeks@gmail.com>
2025-12-23 23:43:49 -0700

    Add OpenAPI documentation plugin

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,41 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'Personal Portfolio API',
+        description: 'API documentation for the personal portfolio application',
+        contact: {
+          name: 'API Support',
+          email: 'pfoxreeks@gmail.com',
+        },
+        license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+        },
+      },
+      'x-strapi-config': {
+        plugins: ['upload', 'users-permissions'],
+        path: '/documentation',
+      },
+      servers: [
+        {
+          url: 'http://localhost:1338/api',
+          description: 'Development server',
+        },
+        {
+          url: 'https://strapi-production-4a26.up.railway.app/api',
+          description: 'Production server',
+        },
+      ],
+      externalDocs: {
+        description: 'Find out more',
+        url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
+      },
+    },
+  },
 });
